c5ff135050fb4025d4d5e010aa3321b136b6d363
Sungwoo Jo <nonswing.z@gmail.com>
2021-07-16 16:36:49 +0900

    Update reports to match newdart

--- dart_fss/filings/pages.py
+++ dart_fss/filings/pages.py
@@ -27,9 +27,9 @@ class Page(object):
 
     """
 
-    _BASE_URL_ = 'http://dart.fss.or.kr/report/viewer.do'
+    _BASE_URL_ = 'http://newdart.fss.or.kr/report/viewer.do'
 
-    def __init__(self, title: str, rcp_no: str, dcm_no: str, ele_id: str,
+    def __init__(self, title: str, rcp_no: str, dcm_no: str, ele_id: int,
                  offset: str, length: str, dtd: str, lazy_loading=True):
         self.title = title
         self.rcp_no = rcp_no
@@ -62,11 +62,11 @@ class Page(object):
             tags = bs.find_all(attrs={tag: re.compile(r'.*')})
             if tags:
                 for t in tags:
-                    t[tag] = "http://dart.fss.or.kr" + t[tag]
+                    t[tag] = "http://newdart.fss.or.kr" + t[tag]
             return bs
 
         def add_prefix(match_obj):
-            return r"window.open('http://dart.fss.or.kr" + match_obj.group(1) + r"'"
+            return r"window.open('http://newdart.fss.or.kr" + match_obj.group(1) + r"'"
 
         payload = {
             'rcpNo': self.rcp_no,
